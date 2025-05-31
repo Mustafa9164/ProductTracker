92f55404a61fc39f461201e0bad2cfce760b5f9f
Mustafa9164 <mohdmustafa9164@gmail.com>
2025-05-31 11:49:02 +0530

    edit and delete feature added in project

--- src/main/java/com/tcs/app/controller/ProductController.java
+++ src/main/java/com/tcs/app/controller/ProductController.java
@@ -1,12 +1,19 @@
 package com.tcs.app.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.print.DocFlavor.STRING;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.tcs.app.entity.Product;
 import com.tcs.app.repository.ProductRepository;
@@ -37,6 +44,33 @@ public class ProductController {
 		Product saveProduct = repository.save(product);
 		model.addAttribute("msg", "Product Saved sucessfully");
 		return "index";
+	}
+	
+	@GetMapping("/editProduct/{id}")
+	public String showEdit(Model model,@PathVariable("id") Integer pid) {
+		 Optional<Product> optional = repository.findById(pid);
+		 if(optional.isPresent()) {
+				model.addAttribute("product", optional.get());
+				return "editProduct";
+		 }
+		
+		return "errorPage";
+	}
+	
+	@PostMapping("/updateProduct")
+	public String handleUpdateProd(@ModelAttribute Product product,Model model) {
+			Product save = repository.save(product);
+			List<Product> allProd = repository.findAll();
+			model.addAttribute("products", allProd);
+			return "data";
+		 }
+	
+	@GetMapping("/deleteProduct/{id}")
+	public String handleDeleteProd(@PathVariable("id") Integer pid,Model model) {
+		repository.deleteById(pid);
+		List<Product> allProd = repository.findAll();
+		model.addAttribute("products", allProd);
+		return "data";
 	}
 
 }
